f9a21080d0556e334bf78b6f74a85c54d0eb5a2c
Harsh <joshiharsh920@gmail.com>
2026-04-06 14:25:14 +0530

    code updates

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -56,7 +56,6 @@ export default async function ProductDetailPage({
                             {product.description}
                         </p>
                     </div>
-
                     <AddToCartButton product={product} />
                 </div>
             </div>
